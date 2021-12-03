ffde951435def7cb37e8b6ada0ace56fc0e4c82b
Nick-Lamanna907 <87162884+Nick-Lamanna907@users.noreply.github.com>
2021-12-03 12:59:04 +1100

    Projects for intermediate course

---
+++ projects/Intermediate/0102e.py
@@ -0,0 +1,16 @@
+goals = 2
+#       ^^ type in the number of goals scored
+
+behinds = 10
+#         ^^ type in the number of behinds scored
+
+________ = ________ * 6 + ________
+
+print("Number of goals scored:")
+print(________)
+
+print("Number of behinds scored:")
+print(________)
+
+print("Total score:")
+print(________)
---
+++ projects/Intermediate/0601e.py
@@ -0,0 +1,10 @@
+whispering = True
+
+while ________________:
+  speech = input("whisper what you'd like to say:")
+
+  if speech.________ ____ ________:
+    print("that's interesting....")
+  else:
+    print("The librarian catches you talking too loudly. Sorry you'll have to leave!")
+    ________
