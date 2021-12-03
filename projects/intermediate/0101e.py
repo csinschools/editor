951265342cd6ce014d79b3635fb6f05cac6fff62
Nick-Lamanna907 <87162884+Nick-Lamanna907@users.noreply.github.com>
2021-12-03 13:01:42 +1100

    intermediate project files

---
+++ projects/intermediate/0101e.py
@@ -0,0 +1,32 @@
+print("I predict your FINAL ANSWER will be...")
+print("ZERO!!!")
+print() # This print() just prints a blank line to the screen!
+
+your_number = 10
+#             ^^ type in your own number between 1 and 20!
+
+# For each of the print() commands below, you need to do some maths! The first one is done for you.
+
+print("Adding 2...")
+number2 = your_number + 2
+
+print("Tripling it...")
+number3 = number2 * ________
+
+print("Subtracting 6...")
+number4 = ________ - 6
+
+print("Dividing by 3...")
+________ = ________ / 3
+
+print("Subtracting the original number...")
+number6 = ________ - ________
+
+print() # This print() just prints a blank line to the screen!
+
+# Print the result to the screen!
+print("I worked out...")
+print(________)
+
+print() # This print() just prints a blank line to the screen!
+print("Was I correct??!")
---
+++ projects/intermediate/0103e.py
@@ -0,0 +1,11 @@
+feet = ________                       
+#        ^^ type in the number of feet in length!
+
+metres = ________
+#        ^^ put your calculation here!
+
+print("Number of feet in length:")
+print(________)
+
+print("Number of metres in length:")
+print(________)
