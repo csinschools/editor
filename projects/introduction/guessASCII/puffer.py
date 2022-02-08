df70e9cb29b11811e7b22ca851a041c203627873
Nicholas Lamanna <nllama@Nicholass-Air.modem>
2022-02-09 09:46:11 +1100

    Guessing ASCII projects added

---
+++ projects/introduction/guessASCII/puffer.py
@@ -0,0 +1,35 @@
+# Move the slider accross so no one can see
+# the ASCII art below.
+#
+# Press "RUN" and reveal one line at a time
+# by pressing "ENTER".
+#
+# Whoever guesses what the ASCII art is first,
+# wins!
+
+from csinsc import *
+
+input("                          A       ;                              ")
+input("                |   ,--,-/ \---,-/|  ,                           ")
+input("               _|\,'. /|      /|   `/|-.                         ")
+input("          \`.'    /|      ,            `;.                       ")
+input("          ,'\   A     A         A   A _ /| `.;                   ")
+input("        ,/  _              A       _  / _   /|  ;                ")
+input("       /\  / \   ,  ,           A  /    /     `/|                ")
+input("      /_| | _ \         ,     ,             ,/  \                ")
+input("     // | |/ `.\  ,-      ,       ,   ,/ ,/      \/              ")
+input("     / @| |@  / /'   \  \      ,              >  /|    ,--.      ")
+input("    |\_/   \_/ /      |  |           ,  ,/        \  ./' __:..   ")
+input("    |  __ __  |       |  | .--.  ,         >  >   |-'   /     `  ")
+input("  ,/| /  '  \ |       |  |     \      ,           |    /         ")
+input(" /  |<--.__,->|       |  | .    `.        >  >    /   (          ")
+input("/_,' \\  ^  /  \     /  /   `.    >--            /^\   |         ")
+input("      \\___/    \   /  /      \__'     \   \   \/   \  |         ")
+input("       `.   |/          ,  ,                  /`\    \  )        ")
+input("         \  '  |/    ,       V    \          /        `-\        ")
+input("          `|/  '  V      V           \    \.'            \_      ")
+input("          '`-.       V       V        \./'\                      ")
+input("               `|/-.      \ /   \ /,---`\         kat            ")
+input("                /   `._____V_____V'                              ")
+input("                           '     '                               ")
+input("It's a pufferfish!")
