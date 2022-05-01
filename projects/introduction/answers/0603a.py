cb9134073fef91ae4c8e8f7f6c036c2327961a82
Zach Wingrave <github@zachwingrave.com>
2022-05-01 14:14:16 +1000

    Update 0603a.py
    Refactored solution to use input instead of a number driven menu.

--- projects/introduction/answers/0603a.py
+++ projects/introduction/answers/0603a.py
@@ -1,32 +1,36 @@
 # Write a program that asks the user for a planet
 # and then displays a fact about that planet.
-# 
+#
 # Keep asking for planets until the user types "quit"!
 
 from csinsc import *
 
 label .menu
 
-print("What would you like to do?")
-print("1. Earth")
-print("2. Mars")
-print("3. Quit")
+print("What's your favourite planet?")
+planet = input("Enter your planet (all lowercase): ")
 
-choice = input("Make youre choice: ")
+if planet == "mercury":
+    print("Mercury is the closest planet to the Sun! Did you know that a year on Mercury is just 88 days long?")
+    goto .menu
 
-if choice == "1":
-  print("Earth is where you live!")
-  goto .menu
-  
-if choice == "2":
-  print("Mars is also known as 'the red planet'")
-  goto .menu
-  
-if choice == "3":
-  goto .end
-  
-print ("Not a valid choice!")
+if planet == "venus":
+    print("Venus is the second planet from the Sun! Did you know that Venus is the third brightest natural object in the sky after the Sun and the Moon?")
+    goto .menu
+
+if planet == "earth":
+    print("That's where we live! Earth is the third planet from the Sun! Did you know that Earth's surface is 70% water?")
+    goto .menu
+
+if planet == "mars":
+    print("Mars is the fourth planet from the Sun! Did you know that the tallest mountain in the Solar System, Olympus Mons, is on Mars and is 21km high? That's almost 13km higher than Mount Everest!")
+    goto .menu
+
+if planet == "quit":
+    goto .end
+
+print ("I've never heard of that planet!")
 goto .menu
 
 label .end
-print("Have a nice day!")
+print("Have a nice day! Don't space out!")
