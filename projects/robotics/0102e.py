efd2e5522a350cb99531a505847bc8bc7c3c36d2
Nicholas Lamanna <nllama@Nicholass-Air.localdomain>
2022-05-30 16:39:00 +1000

    skulpt update, project additions

---
+++ projects/robotics/0102e.py
@@ -0,0 +1,7 @@
+from microbit import *
+
+# Connect to the micro:bit
+m1 = ________()
+
+# Turn on one LED... or more if you want!
+m1.setLED(__, __, ____)
---
+++ projects/robotics/0104e.py
@@ -0,0 +1,18 @@
+from csinsc import *
+from microbit import *
+
+# Connect to the micro:bit
+m1 = ________()
+
+# Create the starting point of the animation
+label .animation
+
+# Turn on an LED - make a cool pattern!
+m1.setLED(__, __, ____)
+# Sleep
+_____(1)
+# Clear the LED screen
+m1.clearLED()
+
+# Go back to the start of the animation
+goto .animation
