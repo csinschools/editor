56800df79edab19ce44e64a9e49eed8674fb6dbf
toan.kien@gmail.com <toan.kien@gmail.com>
2023-03-13 16:46:58 +1100

    Adding projects for learntocode2

---
+++ projects/learntocode2/0101e.py
@@ -0,0 +1,11 @@
+# Change the function calls so that Python speaks the
+# sentences instead of displaying them to the screen.
+
+# Does the punctuation at the end of the sentence
+# make a difference to the speech?
+
+from csinsc import say
+
+print("I love coding.")
+print("I love coding!")
+print("I love coding?")
