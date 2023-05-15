f7b6de4bfa85ce9ff3129c2a0c35aa69221e3961
toan.kien@gmail.com <toan.kien@gmail.com>
2023-05-16 09:47:35 +1000

    Adding new activity - book title out of stock!

---
+++ projects/introduction/answers/0402_2e.py
@@ -0,0 +1,8 @@
+# Fill in the blanks to make a program that asks the user
+# for a book title, then tells them the title is temporarily out of stock
+
+# Fill in the blank below to prompt the user to enter a book title
+book = input ("Which book would you like to buy: ")
+
+# Fill in the blank below so the program says that book is out of stock
+print("Sorry " + book + " is temporarily out of stock!")
