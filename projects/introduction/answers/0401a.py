ffd51dba78ced5981ad97d3b92b78a1db19c0588
toan.kien@gmail.com <toan.kien@gmail.com>
2023-07-22 14:25:15 +1000

    Using 'level' as the variable in 0401e

--- projects/introduction/answers/0401a.py
+++ projects/introduction/answers/0401a.py
@@ -1,7 +1,7 @@
 # Fill in the blank below so the program tells you your year level
 # is the best!
 
-year = input("Enter your year level: ")
+level = input("Enter your year level: ")
 
 # Fill in the blank below to make this program work!
-print("Year " + year + " is the most amazing year level!")
+print("Year " + level + " is the most amazing year level!")
