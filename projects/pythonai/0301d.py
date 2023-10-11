1c568aedc7843aeff8f0bc1e2f9f56a1cbce9031
toan.kien@gmail.com <toan.kien@gmail.com>
2023-10-11 22:16:16 +1100

    Updating cloud variable demo code

---
+++ projects/pythonai/0301d.py
@@ -0,0 +1,6 @@
+from goodies import *
+
+setSchool("school_ID")
+clouddel("cloud_greeting")
+print("Cloud variable cloud_greeting successfully deleted!")
+print("Teachers can now step the students through Student Steps 1, 2 and 3.")
---
+++ projects/pythonai/0302d.py
@@ -0,0 +1,6 @@
+from goodies import *
+
+setSchool("school_ID")
+cloud_greeting = cloudconst("Welcome to the class!")
+print("Cloud variable cloud_greeting successfully assigned a value!")
+print("Teachers can now step the students through Student Steps 4 and 5.")
