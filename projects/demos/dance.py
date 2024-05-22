64ea5451099e9b03ddfac3eec3067c9314ffe3c8
toan.kien@gmail.com <toan.kien@gmail.com>
2024-05-22 23:06:58 +1000

    Added babylon VR and dance pose demo

---
+++ projects/demos/dance.py
@@ -0,0 +1,282 @@
+from pyangelo import *
+from goodies import *
+
+deltaHeight = 20.0
+
+def rightHandRaised(skeleton):
+    if len(skeleton) == 0:
+        return 0
+    scores = skeleton[0]
+    leftWrist = 0
+    leftElbow = 0
+    leftShoulder = 0
+    for score in scores:
+        if score["score"] > 0.5:
+            if score["part"] == "leftShoulder":
+                leftShoulder = score["position"]["y"]
+            elif score["part"] == "leftElbow":
+                leftElbow = score["position"]["y"]
+            elif score["part"] == "leftWrist":
+                leftWrist = score["position"]["y"]
+    if leftShoulder != 0:
+        if leftElbow != 0:
+            if leftWrist != 0:
+                if leftShoulder > leftWrist + deltaHeight:
+                    return 1
+                else:
+                    return -1
+            else:
+                if leftShoulder > leftElbow + deltaHeight:
+                    return 1
+                else:
+                    return -1     
+        else:
+            if leftWrist != 0:
+                if leftShoulder > leftWrist + deltaHeight:
+                    return 1
+                else:
+                    return -1
+            else:
+                return 0
+    else:
+        return 0
+
+def leftHandRaised(skeleton):
+    if len(skeleton) == 0:
+        return 0
+    scores = skeleton[0]
+    rightWrist = 0
+    rightElbow = 0
+    rightShoulder = 0
+    for score in scores:
+        if score["score"] > 0.5:
+            if score["part"] == "rightShoulder":
+                rightShoulder = score["position"]["y"]
+            elif score["part"] == "rightElbow":
+                rightElbow = score["position"]["y"]
+            elif score["part"] == "rightWrist":
+                rightWrist = score["position"]["y"]
+    if rightShoulder != 0:
+        if rightElbow != 0:
+            if rightWrist != 0:
+                if rightShoulder > rightWrist + deltaHeight:
+                    return 1
+                else:
+                    return -1
+            else:
+                if rightShoulder > rightElbow + deltaHeight:
+                    return 1
+                else:
+                    return -1     
+        else:
+            if rightWrist != 0:
+                if rightShoulder > rightWrist + deltaHeight:
+                    return 1
+                else:
+                    return -1
+            else:
+                return 0
+    else:
+        return 0
+        
+showWebCam()
+
+
+# preload images
+drawImage("https://i.imgur.com/eALijkc.png", 0, 0, 0, 0)
+drawImage("https://i.imgur.com/hSpp7He.png", 0, 0, 0, 0)
+drawImage("https://i.imgur.com/AyVObiG.png", 0, 0, 0, 0)
+drawImage("https://i.imgur.com/77JDxe9.png", 0, 0, 0, 0)
+drawImage("https://i.imgur.com/F3E0rXn.png", 0, 0, 0, 0)
+drawImage("https://i.imgur.com/yfxTwfS.png", 0, 0, 0, 0)
+
+print(Colour.cyan + "Loading Pose model, this might take a while, please wait...")
+loadPoseModel("https://teachablemachine.withgoogle.com/models/HurEXJ_4P/")
+print(Colour.purple + "Successfully loaded!")
+
+stabilityCount = 2
+
+leftHands = [0] * stabilityCount
+rightHands = [0] * stabilityCount
+
+rightHandUp = False
+leftHandUp = False
+
+def filterHand(hands):
+    return sum(hands) / len(hands)
+    
+    
+def getHands(debug = False):
+    global leftHands, rightHands, rightHandUp, leftHandUp
+    response = getSkeletonFromWebCam()
+    
+    leftHandResponse = leftHandRaised(response)
+    if leftHandResponse != 0:
+        leftHands.append(leftHandResponse)
+        leftHands = leftHands[1:]
+    
+    rightHandResponse = rightHandRaised(response)
+    if rightHandResponse != 0:
+        rightHands.append(rightHandResponse)
+        rightHands = rightHands[1:]    
+    
+    if filterHand(rightHands) > 0.5:
+        if not rightHandUp:
+            if debug:
+                print(Colour.green + "RIGHT hand raised!")
+            rightHandUp = True
+    elif filterHand(rightHands) < -0.5:
+        if rightHandUp:
+            if debug:
+                print(Colour.blue + "RIGHT hand lowered")
+            rightHandUp = False            
+        
+    if filterHand(leftHands) > 0.5:
+        if not leftHandUp:
+            if debug:
+                print(Colour.yellow + "LEFT hand raised!")
+            leftHandUp = True        
+    elif filterHand(leftHands) < -0.5:
+        if leftHandUp:
+            if debug:
+                print(Colour.brown + "LEFT hand lowered!")
+            leftHandUp = False  
+            
+    finalPoseScore = 0
+    if leftHandUp:
+        finalPoseScore += 1
+    if rightHandUp:
+        finalPoseScore += 2
+        
+    return finalPoseScore
+
+poses = [   [0.0,    0, "https://i.imgur.com/AyVObiG.png"], 
+            [0.080,  0, "https://i.imgur.com/AyVObiG.png"],
+            [1.676,  1, "https://i.imgur.com/77JDxe9.png"],
+            [3.283,  3, "https://i.imgur.com/hSpp7He.png"],
+            [4.097,  2, "https://i.imgur.com/eALijkc.png"],
+            [4.869,  0, "https://i.imgur.com/AyVObiG.png"],
+            [6.497,  3, "https://i.imgur.com/hSpp7He.png"],
+            
+            [8.097,  3, "https://i.imgur.com/F3E0rXn.png"],
+            [8.483,  3, "https://i.imgur.com/yfxTwfS.png"],
+            [8.890,  3, "https://i.imgur.com/F3E0rXn.png"],
+            [9.302,  3, "https://i.imgur.com/yfxTwfS.png"],
+            
+            
+            [9.708,  0, "https://i.imgur.com/AyVObiG.png"],
+            [10.901,  0, "https://i.imgur.com/AyVObiG.png"],
+            ]
+
+label .start_game
+stopSound()
+timeElapsed()
+countDown = 3
+countDown -= timeElapsed()
+while countDown > 0:
+    clearScreen(WHITE)
+    drawImage(poses[0][2], 15, 100, 300, 300)
+    drawText("Get Ready!!", 90, 420, "30px Arial", BLUE)
+    drawText(int((countDown // 1) + 1), 100, 120, "200px Arial", RED)
+    refresh()
+    getSkeletonFromWebCam()
+    countDown -= timeElapsed()
+
+
+playSound("https://csinschools.io/editor/samples/music/Turbo_Outrun_finish.mp3")
+while getSoundCurrentTime() == 0:
+    pass
+
+
+poseIndex = 0
+gracePeriod =0.5
+progress = 0
+progressSpeed = 20
+score = 0
+maxScore = 0
+while True:
+    if progress > 255:
+        progress = 255
+    elif progress < -255:
+        progress = -255
+
+    if progress > 0:
+        clearScreen(255 - progress, 255, 255 - progress, 1)
+    else:
+        clearScreen(255, 255 + progress, 255 + progress, 1)
+        
+    if poseIndex < len(poses) - 1:
+        time = getSoundCurrentTime()
+
+        drawImage(poses[poseIndex][2], 15, 100, 300, 300)
+        #print(time)
+        pose = getHands()
+        
+        if pose == poses[poseIndex][1]:
+            #print(Colour.green + "Got it!")
+            progress += progressSpeed
+            if progress < 255:
+                # don't keep adding score when maxed progress
+                score += 1
+                maxScore += 1
+        else:
+            if (time - poses[poseIndex][0]) < gracePeriod:
+                pass
+                #print(Colour.orange + "<<< Grace backward")  
+            #elif (poses[poseIndex + 1][0] - time) < gracePeriod:
+            #    print(Colour.orange + "Grace forward >>>")  
+            else:
+                #print(Colour.red + "Nope!")    
+                progress -= progressSpeed
+                maxScore += 1
+    
+        if time > poses[poseIndex + 1][0]:
+            poseIndex += 1
+            progress = progress / 2
+    else:
+        break
+
+if progress > 0:
+    r = 255 - progress
+    g = 255
+    b = 255 - progress
+else:
+    r = 255
+    g = 255 + progress
+    b = 255 + progress
+
+    
+while True:
+    
+    clearScreen(r, g, b, 1)
+    r -= 0.0125
+    g -= 0.0125
+    b -= 0.0125
+    
+    if r < 0:
+        r = 0
+    if g < 0:
+        g = 0
+    if b < 0:
+        b = 0
+    
+    if r == 0 and g == 0 and b == 0:
+        break
+    
+    
+score100 = int(round(score / maxScore, 2) * 100)
+while True:
+    
+    clearScreen(0, 0, 0, 1)
+    drawText("Well done!", 100, 340, "30px Arial", WHITE)   
+    drawText("Score: " + str(score100), 100, 280, "30px Arial", YELLOW)    
+    
+    stars = score100 // 20
+    starsString = (stars * "★") + ((5 - stars) * "⚝")
+    drawText(starsString, 105, 220, "30px Arial", YELLOW)    
+    getSkeletonFromWebCam()
+    if isKeyReleased('Enter'):
+        break
+    
+    
+goto .start_game
---
+++ projects/demos/vr.py
@@ -0,0 +1,83 @@
+from babylon import *
+
+sun = Sphere()
+sun.position = [0, 5, 0]
+sun.radius = 2
+
+sphere1 = Sphere()
+sphere1.position = [0, 0, 5]
+sphere2 = Sphere()
+sphere2.position = [0, 2, 5]
+sphere3 = Sphere()
+sphere3.position = [0, 4, 5]
+sphere4 = Sphere()
+sphere4.position = [0, 6, 5]
+
+
+sphere5 = Sphere()
+sphere5.position = [0, 0, -5]
+sphere6 = Sphere()
+sphere6.position = [0, 2, -5]
+sphere7 = Sphere()
+sphere7.position = [0, 4, -5]
+sphere8 = Sphere()
+sphere8.position = [0, 6, -5]
+
+
+sphere9 = Sphere()
+sphere9.position = [5, 0, 0]
+sphere10 = Sphere()
+sphere10.position = [5, 2, 0]
+sphere11 = Sphere()
+sphere11.position = [5, 4, 0]
+sphere12 = Sphere()
+sphere12.position = [5, 6, 0]
+
+
+sphere13 = Sphere()
+sphere13.position = [-5, 0, 0]
+sphere14 = Sphere()
+sphere14.position = [-5, 2, 0]
+sphere15 = Sphere()
+sphere15.position = [-5, 4, 0]
+sphere16 = Sphere()
+sphere16.position = [-5, 6, 0]
+
+earth = Sphere()
+earth.position = [0, -5, 0]
+earth.radius = 3
+
+redMaterial = Material()
+redMaterial.ambientColour = Colour.red
+yellowMaterial = Material()
+yellowMaterial.ambientColour = Colour.yellow
+greenMaterial = Material()
+greenMaterial.ambientColour = Colour.green
+blueMaterial = Material()
+blueMaterial.ambientColour = Colour.blue
+magentaMaterial = Material()
+magentaMaterial.ambientColour = Colour.magenta
+
+sun.material = yellowMaterial
+
+sphere1.material = greenMaterial
+sphere2.material = greenMaterial
+sphere3.material = greenMaterial
+sphere4.material = greenMaterial
+
+sphere5.material = redMaterial
+sphere6.material = redMaterial
+sphere7.material = redMaterial
+sphere8.material = redMaterial
+
+sphere9.material = blueMaterial
+sphere10.material = blueMaterial
+sphere11.material = blueMaterial
+sphere12.material = blueMaterial
+
+sphere13.material = magentaMaterial
+sphere14.material = magentaMaterial
+sphere15.material = magentaMaterial
+sphere16.material = magentaMaterial
+
+startBabylon()
