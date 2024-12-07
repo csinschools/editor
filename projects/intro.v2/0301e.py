55bdd0ce5a1a85d10c248bdef6a7ba4e65970095
Toan Huynh <toan.kien@gmail.com>
2024-12-07 20:23:20 +1100

    fixing rainbow exercise

--- projects/intro.v2/0301e.py
+++ projects/intro.v2/0301e.py
@@ -2,4 +2,4 @@ from _______________ import ________________
 
 print(Colour.red +      "RAINBOW")
 print(Colour.orange +   "  RAINBOW") 
-print(Colour.green +    "    RAINBOW")  
+print(Colour.yellow +    "    RAINBOW")  
